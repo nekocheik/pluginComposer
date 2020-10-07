76fbac8f617fbe1b9723a36e1b11ee02550a7d12
nekocheik <cheik.kone@hetic.net>
2020-10-07 10:19:00 +0200

    feat improve

--- index.php
+++ index.php
@@ -5,22 +5,31 @@ require('./organizer.php');
 $mainComposerFile = json_decode(file_get_contents('composer.lock', './'));
 $dependencies = json_decode(json_encode($mainComposerFile), true)['packages'];
 
+foreach ($dependencies as $dependencie) {
+}
 $underDependances = [];
 
-foreach ($dependencies as $dependencie) {
-    $packageParentName = $dependencie['name'];
-    $packageParentVersion = $dependencie['version'];
-    if ($dependencie['require-dev']) {
-        foreach ($dependencie['require-dev'] as $key => $underDependance) {
-            if (!isset($underDependance[$key])) {
-                $underDependances[$key] = [];
-                $underDependances[$key][$underDependance] = [$packageParentName => '0'];
-            } else {
-                var_dump('ici');
-                $underDependance[$key][$underDependance] = [$packageParentName => '0'];
-            };
-        }
-    }
+
+function formatDependence($dependencies)
+{
+    foreach ($dependencies as $dependencie) {
+        $packageParentName = $dependencie['name'];
+        $packageParentVersion = $dependencie['version'];
+        $dependencie['require-dev'];
+    };
 };
 
-var_dump(json_encode($underDependances, JSON_PRETTY_PRINT));
+function getUnderDependence($dependencie, $packageParentName, $packageParentVersion)
+{
+    $result = [];
+    foreach ($dependencie as $key => $dependencie) {
+        if (!isset($underDependance[$key])) {
+            $result[$key] = [];
+            $result[$key][$underDependance] = [$packageParentName => $packageParentVersion];
+        } else {
+            $result[$key][$underDependance] = [$packageParentName => $packageParentVersion];
+        };
+    }
+}
+
+var_dump(json_encode($dependencies, JSON_PRETTY_PRINT));
